6515cc3d139986b80a438343b6ffb3976d2ca3b3
InesFr <ines23franco@gmail.com>
2025-10-18 18:19:50 +0900

    actualizacion recetas.test

--- tfm_final/tests/Recetas.test.js
+++ tfm_final/tests/Recetas.test.js
@@ -3,22 +3,21 @@ import { mount } from "@vue/test-utils";
 import Recetas from "@/views/Recetas.vue";
 
 describe("Recetas.vue", () => {
+
   it("se renderiza correctamente", () => {
     const wrapper = mount(Recetas);
     expect(wrapper.exists()).toBe(true);
   });
-});
-it("muestra el título de la página", () => {
-  const wrapper = mount(Recetas);
-  expect(wrapper.text()).toContain("Recetas");
-});
 
-it("carga las recetas", async () => {
-  const wrapper = mount(Recetas);
-  await wrapper.findComponent({ name: "recetas" });
-});
+  it("muestra el título de la página", () => {
+    const wrapper = mount(Recetas);
+    expect(wrapper.text()).toContain("Recetas");
+  });
 
-it("error al cargar las recetas", async () => {
-  const wrapper = mount(Recetas);
-  await wrapper.findComponent({ name: "recetas" });
+  it("carga recetas desde la API", async () => {
+    const wrapper = mount(Recetas);
+    await flushPromises();
+    expect(wrapper.html()).toMatch(/titulo|descripcion|Receta/i);
+  });
+
 });
